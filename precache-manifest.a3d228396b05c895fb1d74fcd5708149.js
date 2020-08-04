5478ad4c21e70ee6c34b1d7d910ab1618187ed9a
James <james@Jamess-MacBook-Pro.local>
2020-08-03 17:48:03 -0700

    Updates

---
+++ precache-manifest.a3d228396b05c895fb1d74fcd5708149.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1395a1c335b23d3889383ee37022402d",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "48c1813b2842a045eef0",
+    "url": "/monsters/static/css/main.38479aa3.chunk.css"
+  },
+  {
+    "revision": "f32f02700ffeaf5a2b14",
+    "url": "/monsters/static/js/2.bc3e73c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.bc3e73c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48c1813b2842a045eef0",
+    "url": "/monsters/static/js/main.4cfc2bad.chunk.js"
+  },
+  {
+    "revision": "48a948fffa185d281f13",
+    "url": "/monsters/static/js/runtime-main.23400953.js"
+  }
+]);
